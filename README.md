0d4fbb14b0774a7cfbb7ce35998cd2e638ef21b6
shafin009 <ghostboyshafin@gmail.com>
2022-05-22 23:01:34 +0600

    'server-side-making'

---
+++ README.md
@@ -0,0 +1,5 @@
+
+
+This is the database of my project name 'Covid Vaccine WearHouse'
+
+Live Link of this project- Covid Vaccine WearHouse['']
---
+++ index.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const cors = require('cors');
+
+const { ObjectId } = require('mongodb');
+const app = express();
+var MongoClient = require('mongodb').MongoClient;
+require('dotenv').config();
+const port = process.env.PORT || 5000;
+
+//----Middleware---
+app.use(cors());
+app.use(express.json());
+
+
+
+
+var uri = `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@assignment-12-shard-00-00.sva2y.mongodb.net:27017,assignment-12-shard-00-01.sva2y.mongodb.net:27017,assignment-12-shard-00-02.sva2y.mongodb.net:27017/?ssl=true&replicaSet=atlas-65b2xi-shard-0&authSource=admin&retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri);
+
+async function run() {
+
+    try {
+
+        await client.connect();
+        const toolsCollection = client.db("shafin-car").collection("tools");
+
+
+
+
+
+
+
+
+
+
+    } finally { }
+
+}
+run().catch(console.dir);
+
+
+
+
+
+
+
+
+app.get('/', (req, res) => {
+    res.send('sss car site')
+})
+
+app.listen(port, () => {
+    console.log('running port', port)
+})
+
+
+
+
+
