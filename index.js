a8f9c8a58ade95e1b80f7ed3e6473476f823dc02
shafin009 <ghostboyshafin@gmail.com>
2022-05-23 04:00:42 +0600

    'making-server-api'

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 
 const { ObjectId } = require('mongodb');
 const app = express();
-var MongoClient = require('mongodb').MongoClient;
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
@@ -12,11 +12,9 @@ app.use(cors());
 app.use(express.json());
 
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@assignment-12.sva2y.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-
-var uri = `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@assignment-12-shard-00-00.sva2y.mongodb.net:27017,assignment-12-shard-00-01.sva2y.mongodb.net:27017,assignment-12-shard-00-02.sva2y.mongodb.net:27017/?ssl=true&replicaSet=atlas-65b2xi-shard-0&authSource=admin&retryWrites=true&w=majority`;
-
-const client = new MongoClient(uri);
 
 async function run() {
 
@@ -25,11 +23,21 @@ async function run() {
         await client.connect();
         const toolsCollection = client.db("shafin-car").collection("tools");
 
+        app.get("/tools", async (req, res) => {
 
+            const query = {};
+            const cursor = toolsCollection.find(query);
+            const tools = await cursor.toArray();
+            res.send(tools);
+        });
 
+        app.get("/tools/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const tools = await toolsCollection.findOne(query);
+            res.send(tools);
 
-
-
+        });
 
 
 
